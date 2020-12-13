ac2a926e26679846db3d0d627d08dc5895fedd36
AnTaeWook <55526071+AnTaeWook@users.noreply.github.com>
2020-12-13 18:14:56 +0900

    Add files via upload

---
+++ android_project/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cookandroid.project_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("언제까지더라");
+    }
+}
